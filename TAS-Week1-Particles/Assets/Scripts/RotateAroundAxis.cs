f0740507db4e3606ed69ab6c9f436a447a2590bd
Ricardo Escobar <rescobar713@gmail.com>
2019-02-02 22:59:01 -0500

    initial commit

---
+++ TAS-Week1-Particles/Assets/Scripts/RotateAroundAxis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundAxis : MonoBehaviour
+{
+
+    public float rotateSpeed = 50f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+    }
+}
